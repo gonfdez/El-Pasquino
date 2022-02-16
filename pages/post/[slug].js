0fb8cd62559c0e8b66ed95514a5ee3c1324c8886
gonfdez <gonefdez@gmail.com>
2022-02-16 19:12:40 +0100

    slug.js

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -25,7 +25,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
   // It's important to default the slug so that it doesn't return "undefined"
-  const { slug = "" } = context.params
+  const { slug = "zygmunt-bauman" } = context.params
   const post = await client.fetch(`
     *[_type == "post" && slug.current == $slug][0]{title, "name": author->name}
   `, { slug })

fe02bc5e6238db7cc1177b295077989745afbfb6
gonfdez <gonefdez@gmail.com>
2022-02-17 09:28:35 +0100

    Update [slug].js

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -39,7 +39,7 @@ export const getServerSideProps = async pageContext => {
   }
 
   const query = encodeURIComponent(`*[ _type == "post" && slug.current == "${pageSlug}" ]`);
-  const url = `https://zhxqf9jz.api.sanity.io/v1/data/query/production?query=${query}`;
+  const url = `https://zhxqf9jz.api.sanity.io/v1/data/query/test?query=${query}`;
 
   const result = await fetch(url).then(res => res.json());
   const post = result.result[0];
